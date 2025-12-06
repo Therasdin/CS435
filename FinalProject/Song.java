1835629828d555cbc8fa841ac133bf72d3a99543
Torn23 <77405403+Torn23@users.noreply.github.com>
2025-12-06 10:34:41 -0700

    Update Song.java
    makes sure it doesn't crash as much

--- FinalProject/Song.java
+++ FinalProject/Song.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
  */
 public class Song implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private String title;
     private String tag;
     private String artist;
